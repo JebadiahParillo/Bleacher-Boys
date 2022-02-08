b1761df2afa4d4aff63d444b4eb129e25d7e0f12
JebadiahParillo <84472401+JebadiahParillo@users.noreply.github.com>
2022-02-08 14:17:32 -0500

    Add files via upload

---
+++ src/components/homepage.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import 'react-ig-feed/dist/index.css'
+import Tabbar from './tabbar';
+
+
+
+
+
+function Homepage() {
+    return(
+    <div>
+        <Tabbar/>
+    </div>
+  );
+}
+export default Homepage;
